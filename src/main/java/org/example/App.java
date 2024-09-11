40359da493c985674847f03e1d6e8b236eca3fd8
dingminzhen <dingminzhen@aspirecn.com>
2024-09-11 18:16:18 +0800

    第一次提交

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import java.util.ArrayList;
+
+/**
+ * Hello world!
+ */
+public class App {
+    private static int i;
+    public static void main(String[] args) {
+        System.out.println(1/0);
+        ArrayList<Integer> arrayList = new ArrayList();
+        for (i = 0; i < 100; i++) {
+            arrayList.add(i);
+        }
+        arrayList.stream().filter(x -> x % 2 == 0).forEach(System.out::println);
+//        multiThreadTest();
+    }
+
+    public static void multiThreadTest(){
+        System.out.println("0 main start");
+        new Thread(() -> System.out.println("1"), "thread1").start();
+
+        new Thread(() -> System.out.println("2"), "thread2").start();
+
+        System.out.println("3 main end");
+    }
+}
